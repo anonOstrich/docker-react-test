f65863a3fd8d0bc77e7295139e383509158f6c3c
Jesper Kuutti <jesper.kuutti@gmail.com>
2022-05-12 14:18:02 +0300

    Changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there :3. Tada!
+          Feature: change the textt
         </p>
         <a
           className="App-link"
